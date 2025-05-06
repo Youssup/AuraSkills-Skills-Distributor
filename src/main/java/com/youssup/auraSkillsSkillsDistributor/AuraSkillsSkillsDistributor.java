bd7e8e7051305f74fe379de18abb8e4509f339a3
Youssup <youssupsong@gmail.com>
2025-05-06 14:57:26 -0400

    Testing Event Listeners

--- src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
+++ src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
@@ -1,13 +1,23 @@
 package com.youssup.auraSkillsSkillsDistributor;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AuraSkillsSkillsDistributor extends JavaPlugin {
+public final class AuraSkillsSkillsDistributor extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("AuraSkillsSkillsDistributor plugin has started");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        System.out.println("A player has joined the server");
     }
 
     @Override
