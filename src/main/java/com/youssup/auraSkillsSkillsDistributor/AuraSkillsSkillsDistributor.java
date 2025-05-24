139a77169f1e746b254b6f4a6e2361b98029490f
Yoosup Song <Youssupsong@gmail.com>
2025-05-24 08:55:29 -0400

    feat: created SkillPointManager class

--- src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
+++ src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
@@ -1,28 +1,18 @@
 package com.youssup.auraSkillsSkillsDistributor;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AuraSkillsSkillsDistributor extends JavaPlugin implements Listener {
+public final class AuraSkillsSkillsDistributor extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("AuraSkillsSkillsDistributor plugin has started");
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
-        event.setJoinMessage("unfortunately "+event.getPlayer()+" has joined the server");
+        System.out.println("AuraSkillsSkillsDistributor plugin by Youssup has started");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("AuraSkillsSkillsDistributor plugin has stopped");
+        System.out.println("AuraSkillsSkillsDistributor plugin by Youssup has stopped");
     }
 }
---
+++ src/main/java/com/youssup/auraSkillsSkillsDistributor/SkillPointManager.java
@@ -0,0 +1,10 @@
+package com.youssup.auraSkillsSkillsDistributor;
+
+import dev.aurelium.auraskills.api.AuraSkillsApi;
+import org.bukkit.event.Listener;
+
+// Manages Skills for each player
+public class SkillPointManager implements Listener {
+
+
+}
