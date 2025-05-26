65b1447ee2201c5307457adfdf76e056b71f4260
Youssup <youssupsong@gmail.com>
2025-05-26 13:31:28 -0400

    feat: pass the plugin instance to SkillPointManager and get the API instance for AuraSkills

--- src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
+++ src/main/java/com/youssup/auraSkillsSkillsDistributor/AuraSkillsSkillsDistributor.java
@@ -4,15 +4,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AuraSkillsSkillsDistributor extends JavaPlugin {
 
+    private static AuraSkillsSkillsDistributor plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("AuraSkillsSkillsDistributor plugin by Youssup has started");
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         System.out.println("AuraSkillsSkillsDistributor plugin by Youssup has stopped");
+    }
+
+    public static AuraSkillsSkillsDistributor getPlugin() {
+        return plugin;
     }
 }
--- src/main/java/com/youssup/auraSkillsSkillsDistributor/SkillPointManager.java
+++ src/main/java/com/youssup/auraSkillsSkillsDistributor/SkillPointManager.java
@@ -1,5 +1,7 @@
 package com.youssup.auraSkillsSkillsDistributor;
 
+import dev.aurelium.auraskills.api.AuraSkillsApi;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 import java.util.*;
@@ -8,4 +10,13 @@ import java.util.*;
 public class SkillPointManager implements Listener {
     // Track each player's unspent points by UUID
     private final Map<UUID, Integer> unspentPoints = new HashMap<>();
+    private final AuraSkillsApi auraSkills;
+
+    public SkillPointManager() {
+        // Register this class as an event listener
+        Bukkit.getPluginManager().registerEvents(this, AuraSkillsSkillsDistributor.getPlugin());
+
+        // Get AuraSkills API instance
+        this.auraSkills = AuraSkillsApi.get();
+    }
 }
